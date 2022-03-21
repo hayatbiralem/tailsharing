e8dfa62d4828c57e70d55a037b759d6261b35741
Ömür Yanıkoğlu <hayatbiralem@gmail.com>
2022-03-21 03:53:20 +0300

    fix twitter link template

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ function page() {
         id: "twitter",
         name: "Twitter",
         path: "m459 152 1 13c0 139-106 299-299 299-59 0-115-17-161-47a217 217 0 0 0 156-44c-47-1-85-31-98-72l19 1c10 0 19-1 28-3-48-10-84-52-84-103v-2c14 8 30 13 47 14A105 105 0 0 1 36 67c51 64 129 106 216 110-2-8-2-16-2-24a105 105 0 0 1 181-72c24-4 47-13 67-25-8 24-25 45-46 58 21-3 41-8 60-17-14 21-32 40-53 55z",
-        template: "https://twitter.com/share?url={url}&text={text}",
+        template: "https://twitter.com/intent/tweet?url={url}&text={text}",
         selected: true,
         extendedLabelPrefix: "Share on",
       },
